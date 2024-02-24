3bd9061b0c77372216b551b7b24264a7ffd046ee
Catarino Carmona <carmonc1@gmatc.matc.edu>
2024-02-23 19:38:19 -0600

    test

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/MatcSpring24/Itdev164-900-WebPrograming2/Catarino-CarmonaLorenzo/.cache/dev-404-page.js"))
+}
+
