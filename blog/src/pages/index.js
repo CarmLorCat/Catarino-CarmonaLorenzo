acb0f0ab4a4fa8735c44ff48abd41f42f2bcf2c3
Catarino Carmona <carmonc1@gmatc.matc.edu>
2024-02-28 19:27:00 -0600

    Update index.js to display the image and excerpt

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -15,7 +15,15 @@ const IndexPage = ({data})=>(
     data.allContentfulBlogPost.edges.map(edge=>(
       <li key={edge.node.id}>
       <Link to={edge.node.slug}>{edge.node.title}</Link>
+      <div>
+      <GatsbyImage
+    image={edge.node.heroImage.gatsbyImageData}/>
+      </div>
+      <div>
+      {edge.node.body.childMarkdownRemark.excerpt}<div/>
+      </div>
     </li>
+    
       ))
   }
   </ul>
@@ -26,13 +34,25 @@ export const Head = ()=><Seo title="Home"/>
 export default IndexPage
 
 export const query = graphql`
-{
+ {
   allContentfulBlogPost{
     edges{
       node{
         id
         title
         slug
+        body{
+          childMarkdownRemark{
+            excerpt
+          }
+        }
+        heroImage{
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: BLURRED
+            width: 300
+          )
+        }
       }
     }
   }
