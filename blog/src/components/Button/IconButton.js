0ae740de3b58fbfd0e9792fa177844ba158199de
Catarino Carmona <carmonc1@gmatc.matc.edu>
2024-04-15 16:09:51 -0500

    bugs

--- blog/src/components/Button/IconButton.js
+++ blog/src/components/Button/IconButton.js
@@ -9,7 +9,7 @@ const StyledButton = styled(BaseButton)`
 ${({theme, variant}) => theme.variants.iconButton[variant || 'primary']}
 
 
-export const IconButton =styled(({icon, ...rest})=>{
+export const IconButton = styled(({icon, ...rest})=>{
     let clone= React.cloneElement(icon, rest)
     return <StyledButton {...rest} className={rest.className}>{clone}</StyledButton>
 })`
