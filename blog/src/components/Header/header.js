61f56b960831348b4d30990bd1d528a39235ae9e
Catarino Carmona <carmonc1@gmatc.matc.edu>
2024-04-17 19:59:36 -0500

    Install components in Header.js file

--- blog/src/components/Header/header.js
+++ blog/src/components/Header/header.js
@@ -16,10 +16,35 @@ justify-content: space-between;
 height: 50px;
 background: ${({theme})=> theme.variants.header.primary.backgroundColor};
 `
+//const StyledLink = styled(Link)`
+//font-size: var(--font-sm);
+//text-decoration: none;
+//color: ${({theme})=> theme.variants.header.primary.color};
+//`
+
 const StyledLink = styled(Link)`
-font-size: var(--font-sm);
-text-decoration: none;
-color: ${({theme})=> theme.variants.header.primary.color};
+color: white;
+text-decoration:none;
+margin:0 10px;
+&:hover {
+  color: lightgray:
+}
+`
+const Image = styled.img`
+margin: 0;
+`
+const Nav = styled(Flex)`
+flex-direction: row;
+justify-content: flex-end;
+align-items: center;
+`
+const Title = styled(H1)`
+flex: 4;
+`
+const MediaQuery = styled.div`
+@media (max-width: 600px) {
+  display: none;
+}
 `
 const Header = ({ siteTitle }) => (
   <StyledHeader>
